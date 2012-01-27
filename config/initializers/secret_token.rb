c37ddade5d820cdbf79ca9a804bf64b5e2efeb26
Sam Rose <samuel.rose@gmail.com>
2012-01-27 15:27:28 -0500

    admin interface and user with profile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vrlcommunitycurrency::Application.config.secret_token = 'e5c770cc0df5e29f875d5fe0d4dc12fb6412104f2be4a3144567440a6e22eb292dceaaa2f72b70b2b9fba103feb365db053bda65ab6f9c0f4db102c36beca38f'
